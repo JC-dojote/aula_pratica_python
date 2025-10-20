cd89b7577352f6badb760734ee6c332ff43a2d27
Jean C. A. Amaral <103057339+JC-dojote@users.noreply.github.com>
2025-10-20 20:57:13 -0300

    Continuação
    Fix constructor method name from __int__ to __init__ and add docstring.

--- atividade2.py
+++ atividade2.py
@@ -4,4 +4,12 @@ class Circulo:
     #Passo 2: Criar uma constante estática de PI
     PI = math.pi
 
+
     #Passo 3: Criar o construtor padrão e os atributos
+    def __int__(self, raio=1.0, cor="Branco"):
+        """
+        Construtor padrão. Se nenhum valor for passado, assume raio=1.0 e cor=Branco
+        """
+        self.raio = raio # Atributo raio
+        self.cor = cor # Atributo cor
+        self.circunference = self.calcCircunferencia() # Calcula a circunferência ao iniciar 
